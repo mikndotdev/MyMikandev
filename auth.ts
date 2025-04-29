c8d4c91d94f2c85612682448b187558fbcfb59ab
maamokun/MikanDev <sachadeco214@gmail.com>
2025-04-29 23:32:13 +0900

    wip: refactor with better UI

--- auth.ts
+++ auth.ts
@@ -15,11 +15,13 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
 		async jwt({ token, account }) {
 			if (account) {
 				token.accessToken = account.access_token;
+
 			}
 			return token;
 		},
 		async session({ session, token }) {
 			console.log(token);
+			session.user.id = token.sub as string;
 			session.accessToken = token.accessToken;
 			return session;
 		},
